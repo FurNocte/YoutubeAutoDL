8456edb42d3b368bb6ced3a2c2dd5ccfce609e97
Fur Nocte <fur.nocte@gmail.com>
2016-07-27 12:11:53 +0200

    Create dir if not exist and auto file extension

--- dloader.js
+++ dloader.js
@@ -2,41 +2,57 @@ var youtubedl = require('youtube-dl');
 var fs = require('fs');
 
 function getVideo(videoSnippet) {
-    var url = videoSnippet.resourceId.videoId;
-    var title = videoSnippet.title;
-    var video = youtubedl('http://www.youtube.com/watch?v=' + url,
-            // Optional arguments passed to youtube-dl.
-            ['-f bestvideo+bestaudio'],
-            // Additional options can be given for calling `child_process.execFile()`.
-            { cwd: __dirname });
+    fs.access(__dirname + '/videos/', function(err) {
+        if (err) {
+            fs.mkdir(__dirname + '/videos/', function() {
+                getVideo(videoSnippet);
+            });
+            return;
+        }
+        var url = videoSnippet.resourceId.videoId;
+        var title = videoSnippet.title;
+        var extension = '';
+        var video = youtubedl('http://www.youtube.com/watch?v=' + url,
+                // Optional arguments passed to youtube-dl.
+                ['-f best'],
+                // Additional options can be given for calling `child_process.execFile()`.
+                { cwd: __dirname });
 
-    // Will be called when the download starts.
-    video.on('info', function(info) {
-        console.log('Download started');
-        console.log('filename: ' + info._filename);
-        console.log('size: ' + info.size);
+        // Will be called when the download starts.
+        video.on('info', function(info) {
+            extension = info._filename.match(/\..{3,4}$/)[0];
+            console.log('Download started');
+            console.log('filename: ' + info._filename);
+            console.log('size: ' + info.size);
+            video.pipe(fs.createWriteStream(__dirname + '/videos/' + title + extension));
+        });
     });
-
-    video.pipe(fs.createWriteStream('./videos/' + title + '.mp4'));
 }
 
 function getMusic(musicSnippet) {
-    var url = musicSnippet.resourceId.videoId;
-    var title = musicSnippet.title;
-    var video = youtubedl('http://www.youtube.com/watch?v=' + url,
-            // Optional arguments passed to youtube-dl.
-            ['-f 140'],
-            // Additional options can be given for calling `child_process.execFile()`.
-            { cwd: __dirname });
+    fs.access(__dirname + '/musics/', function(err) {
+        if (err) {
+            fs.mkdir(__dirname + '/musics/', function() {
+                getMusic(musicSnippet);
+            });
+            return;
+        }
+        var url = musicSnippet.resourceId.videoId;
+        var title = musicSnippet.title;
+        var video = youtubedl('http://www.youtube.com/watch?v=' + url,
+                // Optional arguments passed to youtube-dl.
+                ['-f 140'],
+                // Additional options can be given for calling `child_process.execFile()`.
+                { cwd: __dirname });
 
-    // Will be called when the download starts.
-    video.on('info', function(info) {
-        console.log('Download started');
-        console.log('filename: ' + info._filename);
-        console.log('size: ' + info.size);
+        // Will be called when the download starts.
+        video.on('info', function(info) {
+            console.log('Download started');
+            console.log('filename: ' + info._filename);
+            console.log('size: ' + info.size);
+            video.pipe(fs.createWriteStream(__dirname + '/musics/' + title + '.m4a'));
+        });
     });
-
-    video.pipe(fs.createWriteStream('./musics/' + title + '.m4a'));
 }
 
 exports.getVideo = getVideo;
